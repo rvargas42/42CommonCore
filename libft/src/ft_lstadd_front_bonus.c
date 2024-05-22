fd7d44ab3d99fb071507ac2f3e56349eab0aa7c1
Ramón Vargas <ravargas@student.42madrid.com>
2024-05-22 13:50:51 +0200

    Add files via upload

---
+++ libft/src/ft_lstadd_front_bonus.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ravargas <ravargas@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 11:16:29 by ravargas          #+#    #+#             */
+/*   Updated: 2024/05/15 13:12:55 by ravargas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst && new)
+		new -> next = *lst;
+	*lst = new;
+}
---
+++ libft/src/ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ravargas <ravargas@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 14:41:41 by ravargas          #+#    #+#             */
+/*   Updated: 2024/05/14 13:23:06 by ravargas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
+
+	str = (char *)s;
+	i = ft_strlen(str);
+	if (c == 0)
+		return ((char *)s + i);
+	while (i >= 0)
+	{
+		if (s[i] == (char) c)
+			return ((char *)(s + i));
+		i--;
+	}
+	return (NULL);
+}
