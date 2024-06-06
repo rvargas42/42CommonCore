37f264b77de7059dc86ba45e89c68e5f5d8e712b
rvargas42 <ravargas.42t@gmail.com>
2024-06-06 20:25:19 +0200

    push_swap: updated duplicate functions check and print utils

--- push_swap/src/algorithms/test_instructions.c
+++ push_swap/src/algorithms/test_instructions.c
@@ -6,7 +6,7 @@
 /*   By: ravargas <ravargas@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 11:07:55 by ravargas          #+#    #+#             */
-/*   Updated: 2024/06/06 18:22:37 by ravargas         ###   ########.fr       */
+/*   Updated: 2024/06/06 18:24:31 by ravargas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ void	test_instructions(t_stacks *stacks)
 	i = 12;
 	while (i != 0)
 	{
-		push_stack(stacks->a, stacks->b);
-		swap_stack(stacks->a);
+		//push_stack(stacks->a, stacks->b);
+		//swap_stack(stacks->a);
 		//rotate_stack(stacks->a);
-		//reverse_rotate(stacks->a);
+		reverse_rotate(stacks->a);
 		ft_printf("\n");
 		ft_printf("a_entries: %d, a_head: %d, a_tail: %d\n", stacks->a->entries, stacks->a->head, stacks->a->tail);
 		ft_printf("b_entries: %d, b_head: %d, b_tail: %d\n", stacks->b->entries, stacks->b->head, stacks->b->tail);
